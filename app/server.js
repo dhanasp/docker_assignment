c12215578b06c058c894db3783c21709b9e01a04
Dhnashri <in-dhnashrip@Dhnashris-MacBook-Pro-2.local>
2019-02-12 19:54:19 +0530

    Add post api to save todos in todos.json

--- app/server.js
+++ app/server.js
@@ -2,41 +2,46 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const request = require('request');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', (req, res) => res.send(
     `<p>Hello<p>`
 ));
 
 const getHtmlTodo = (parsedTodos) => {
-    let todosHtml = parsedTodos.map((todo) => (`<li><p>Todo ${todo["task"]}</p> status : ${todo["done"]}<li>`)).join("");
+    let todosHtml = parsedTodos.map((todo) => (`<li><p>Todo Title: ${todo["name"]}</p> status : ${todo["status"]}</li>`)).join("");
     return `<h5>Your Todos:</h5><ul>${todosHtml}</ul>`;
 }
 
 
-app.get('/todos', (req, res) => {
+app.get('/todo', (req, res) => {
     let reqUrl = `http://${process.env.DNS_NAME}:${process.env.PORT}${req.url}`;
-
     let inputForTodo = " <h5>Please Enter todo:</h5> " +
-        "<form action='/save-todo' method='post'>  " +
-        "Name: <input type='text' name='name'><br> status : <input type='text' name='details'><br> <input type='submit' value='Submit'><br> " +
+        "<form action='/todo' method='post'>  " +
+        "Name: <input type='text' name='name'><br> status : <input type='text' name='status'><br> <input type='submit' value='Submit'><br> " +
         "</form><br> ";
 
     request.get({url: reqUrl},
         function (err, response, body) {
-        if(body){res.send(inputForTodo + getHtmlTodo(JSON.parse(body)))}
-        if (err) return err;
+            if (body) {
+                res.send(inputForTodo + getHtmlTodo(JSON.parse(body)))
+            }
+            if (err) return err;
         })
 });
 
 
-app.post('/save-todo', (req, res) => {
+app.post('/todo', (req, res) => {
     let reqUrl = `http://${process.env.DNS_NAME}:${process.env.PORT}${req.url}`;
-    console.log(reqUrl);
-    request.post({url: reqUrl}, {form: req.body},
-        function (err, response, body) {
+    const todo ={name: req.body.name, status: req.body.status};
+    request.post({url:reqUrl,json:todo}, function(error, response, body) {
             res.send(body);
-        }
-    )
+        });
 });
 
 
--- todo/src/main/java/com/example/todo/TodoApplication.java
+++ todo/src/main/java/com/example/todo/TodoApplication.java
@@ -1,13 +1,19 @@
 package com.example.todo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 
 @SpringBootApplication
 @RestController
@@ -19,19 +25,31 @@ public class TodoApplication {
     }
 
     private String readTodos() throws IOException {
-        String pwd = System.getProperty("user.dir");
-        String pathToFile = String.format("%s/todos.json", pwd);
-        return new String(Files.readAllBytes(Paths.get(pathToFile)));
+        return new String(Files.readAllBytes(getPath()));
     }
 
-    @RequestMapping("/todos")
+    @RequestMapping("/todo")
     public String getTodoContent() throws IOException {
         return readTodos();
+    }
+
+    @PostMapping(path = "/todo")
+    public String saveTodo(@RequestBody Object todo) throws IOException, JSONException {
+        JSONArray todoContent = new JSONArray(readTodos());
+        todoContent.put(todo);
+        Path filename = getPath();
+        Files.write(filename, Collections.singleton(todoContent.toString()));
+        return todo.toString();
+    }
+
+    private Path getPath() {
+        return Paths.get(System.getProperty("user.dir") + "/todos.json");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
     }
+
 
 }
 
